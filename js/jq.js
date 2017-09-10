1bcab08fde30b160b8bec12782967cc6b58395ab
lecopzer <james455096@gmail.com>
2017-09-10 22:45:38 +0800

    Correct tab size

--- js/jq.js
+++ js/jq.js
@@ -1,92 +1,94 @@
 
 $(function() {
-	$("#sliderSize").slider({
-		range: "min",
-		min: 50,
-		max: 100,
-		value: 75,
-		slide: function(event, ui) {
-			$("#main").css({ "left": (100 - ui.value) / 2 + "%",
-							 "width": ui.value + "%"});
-			$("#tableSize").val(ui.value + "%");
-		}
-	});
-	$("#tableSize").val( $("#sliderSize").slider("value") + "%");
+  $("#sliderSize").slider({
+    range: "min",
+    min: 50,
+    max: 100,
+    value: 75,
+    slide: function(event, ui) {
+      $("#main").css({ "left": (100 - ui.value) / 2 + "%",
+               "width": ui.value + "%"});
+      $("#tableSize").val(ui.value + "%");
+    }
+  });
+  $("#tableSize").val( $("#sliderSize").slider("value") + "%");
 });
 
 $(function() {
-//	$("#classList").sortable();
-	$("#navAddFloat").draggable();
-	$("#tabs").tabs();
+//  $("#classList").sortable();
+  $("#navAddFloat").draggable();
+  $("#tabs").tabs();
 });
 
 
 $(function () {
-	$("header ul li:gt(2):lt(2) a").click(function(e) {
-		$.ajaxSetup({
-			cache: false,
-		});
-		$("#mainpage").load( $(this).attr("href"), function(){
-		});
-		return false;
-	});
+  $("header ul li:gt(2):lt(2) a").click(function(e) {
+    $.ajaxSetup({
+      cache: false,
+    });
+    $("#mainpage").load( $(this).attr("href"), function(){
+    });
+    return false;
+  });
 });
 
 
 
 $(function() {
-	$("#classQuery").tooltip({
-		disabled: true,
-		content: "希望有生之年可以完成他ㄏㄏ",
-		items: "li",
-		position: {
-			my: "center top+30",
-			at: "center down",
-			using: function (position, feedback) {
-				$(this).css(position);            
-				$("<div>")
-					.addClass( "arrow" )
-					.addClass( feedback.vertical )
-					.addClass( feedback.horizontal )
-					.appendTo( this );
-			}
-		},
-		show: {
-			effect: "slideDown",
-		}
-	});
+  $("#classQuery").tooltip({
+    disabled: true,
+    content: "希望有生之年可以完成他ㄏㄏ",
+    items: "li",
+    position: {
+      my: "center top+30",
+      at: "center down",
+      using: function (position, feedback) {
+        $(this).css(position);            
+        $("<div>")
+          .addClass( "arrow" )
+          .addClass( feedback.vertical )
+          .addClass( feedback.horizontal )
+          .appendTo( this );
+      }
+    },
+    show: {
+      effect: "slideDown",
+    }
+  });
 
-  /*
-	$(document).on("click", "#classQuery", function(e) {
-		e.preventDefault();
-		$("#navSearchPanel").css("display","inline");
-	});
-*/
-	$(document).on({
-		mouseenter: function() {
-			registerInfo($(this).attr("id"));
-		},
-		mouseleave: function() {
-			$("#cellInfo").fadeOut("fast");
-			
-		},	
-		mousemove: function(e) {
-			$("#cellInfo").show();
-			$("#cellInfo").css({
-				top: e.pageY,
-				left: e.pageX
-			});
-			$("#cellInfo").animate({
-				top: e.pageY,
-				left: e.pageX	
-			},
-			{
-				queue: false,
-				duration: 200,
-				easing: "linear",
-			}
-								  
-								  );
-		}
-	}, ".cell");
+  $(document).on("click", "#classQuery", function(e) {
+    e.preventDefault();
+    $("#navSearchPanel").toggle("slide", null, 500, callbackNull);
+  });
+
+  function callbackNull() {
+  };
+  
+  $(document).on({
+    mouseenter: function() {
+      registerInfo($(this).attr("id"));
+    },
+    mouseleave: function() {
+      $("#cellInfo").fadeOut("fast");
+      
+    },  
+    mousemove: function(e) {
+      $("#cellInfo").show();
+      $("#cellInfo").css({
+        top: e.pageY,
+        left: e.pageX
+      });
+      $("#cellInfo").animate({
+        top: e.pageY,
+        left: e.pageX 
+      },
+      {
+        queue: false,
+        duration: 200,
+        easing: "linear",
+      }
+                  
+                  );
+    }
+  }, ".cell");
  });
